03b934211dc73a7b13b1f4e38ec6e8599b65e00e
Jay <80186604+jay-fivem@users.noreply.github.com>
2021-11-29 21:41:04 +1030

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -1,12 +1,1 @@
-["bong"] = {
-    ["name"] = "bong",
-    ["label"] = "Bong",
-    ["weight"] = 500,
-    ["type"] = "item",
-    ["image"] = "bong.png",
-    ["unique"] = false,
-    ["useable"] = true,
-    ["shouldClose"] = true,
-    ["combinable"] = nil,
-    ["description"] = "In Australia this is called a \"Billy\""
-},
+["bong"]                               = { ["name"] = "bong",                              ["label"] = "Bong",                 ["weight"] = 500,               ["type"] = "item",      ["image"] = "bong.png",                       ["unique"] = false,      ["useable"] = true, ["shouldClose"] = true,             ["combinable"] = nil, ["description"] = "In Australia this is called a \"Billy\""},
