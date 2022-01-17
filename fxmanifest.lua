ce5941cd17dea16cb8e717275f8460cde0079989
ElkieD <69970693+elkie1991gaming@users.noreply.github.com>
2022-01-17 11:18:35 +0000

    SQL Update
    updated for oxmysql 1.9 and above

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,10 @@ fx_version 'bodacious'
 game 'gta5'
 lua54 'yes'
 
-server_script 'server/main.lua'
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
+
 client_script 'client/main.lua'
 shared_script 'config.lua'
